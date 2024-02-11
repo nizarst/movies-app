5e3cc5efbbb8b52ed719b3a06eed4a497fb73720
ismailovic77 <ismail03042000@gmail.com>
2024-02-11 16:12:25 +0100

    removing the repetitive api calls and optimizing the code

--- app/src/main/java/com/landfathich/dreamfilm/MainActivity.kt
+++ app/src/main/java/com/landfathich/dreamfilm/MainActivity.kt
@@ -18,12 +18,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val repo = MovieRepo()
+
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadFragment(HomeFragment(this))
+
+        val callbackConf: () -> Unit = {
+            val callbacktrend: () -> Unit = {
+                val callbackPop: () -> Unit = {
+                    loadFragment(HomeFragment(this))
+                }
+                repo.fetchPopularMoviesFromTMDB("f822ee258fb1f07774b09e5ba51e04e7" , callbackPop)
+            }
+            repo.fetchTrendingMoviesFromTMDB("f822ee258fb1f07774b09e5ba51e04e7" , callbacktrend)
+        }
+        repo.fetchTmdbConfig("f822ee258fb1f07774b09e5ba51e04e7" ,callbackConf)
+
+        //repo.fetchPopularMoviesFromTMDB("f822ee258fb1f07774b09e5ba51e04e7")
+        //loadFragment(HomeFragment(this))
 
         //geth the navigation view
         val navigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
@@ -32,10 +46,12 @@ class MainActivity : AppCompatActivity() {
             {
                 R.id.Home_navigation -> {
                     loadFragment(HomeFragment(this))
+                    Log.d(" home_fragement ", "git into the home menu")
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.Favorites_navigation -> {
                     loadFragment(FavoritesFragment(this))
+                    Log.d(" fav_fragement ", "git into the fav menu")
                     return@setOnNavigationItemSelectedListener true
                 }
                 else -> false
@@ -43,14 +59,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun loadFragment(fragment: Fragment){
+        Log.d(" load_function ", fragment.toString())
         val myCallback: () -> Unit = {
             val transaction = supportFragmentManager.beginTransaction()
-            Log.d("callback_function ", fragment.toString())
+            Log.d(" callback_function ", fragment.toString())
             transaction.replace(R.id.fragment_container , fragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
-        repo.fetchTmdbConfig("f822ee258fb1f07774b09e5ba51e04e7")
+        //repo.fetchTmdbConfig("f822ee258fb1f07774b09e5ba51e04e7")
         repo.PopularMovies(myCallback)
         /*
         if (fragment is HomeFragment) {
